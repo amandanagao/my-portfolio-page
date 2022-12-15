5809b08bcbe210de29d6da01f252f7d065b908da
amandanagao <nagaoamanda@gmail.com>
2022-12-15 07:58:17 -0300

    Added Day

--- src/MenuDate.js
+++ src/MenuDate.js
@@ -3,14 +3,24 @@ import React from "react";
 import "./MenuDate.css";
 
 export default function MenuDate() {
-    const date = new Date();
-    const day = ("0" + date.getDay()).slice(-2);
-    const month = ("0" + (date.getMonth() + 1)).slice(-2);
-    const year = date.getFullYear();
+    const today = new Date();
+    const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    const day = days[today.getDay()];
+    const date = ("0" + today.getDay()).slice(-2);
+    const month = ("0" + (today.getMonth() + 1)).slice(-2);
+    const year = today.getFullYear();
 
     return (
         <div className="MenuDate">
-            {day}.{month}.{year}
+            {day}, {date}.{month}.{year}
         </div>
     );
 }
