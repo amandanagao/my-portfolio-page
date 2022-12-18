18feca59aca08990cc2d65dd5a354fe5b5435f9c
amandanagao <nagaoamanda@gmail.com>
2022-12-18 10:33:30 -0300

    Updated AboutMe component

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -7,12 +7,34 @@ export default function AboutMe() {
         <div className="AboutMe">
             <h4>About Me</h4>
             <p>
-                blibu blabu blibu blabu blibu blabu blibu blabu blibu blabu
-                blibu blabu blibu blabu blibu blabu blibu blabu blibu blabu{" "}
-                blibu blabu blibu blabu blibu blabu blibu blabu blibu blabu{" "}
-                blibu blabu blibu blabu blibu blabu blibu blabu blibu blabu{" "}
-                blibu blabu blibu blabu blibu blabu blibu blabu blibu blabu{" "}
-                blibu blabu blibu blabu blibu blabu blibu
+                <strong>
+                    New to coding but excited to learn more and build a career
+                    in the IT world.
+                </strong>
+                <br />
+                Graduated in Law in 2015, after working 3 years as a lawyer, I
+                came to the conclusion that this area wasn't meant for me. I
+                wasn't happy. So I decided to look for other roles.
+                <br />
+                In 2020, I moved to Dublin, Ireland, with my husband, to study
+                and improve my English skills. I had the opportunity to work in
+                a bakery, as a confectioner. I liked working there but, still,
+                it wasn't what I was looking for as a job to pursue a career.
+                <br />
+                In January 2022, another opportunity appeared, to move to
+                Funchal, Madeira Island, Portugal, where we are currently
+                living. I kept looking for a job and, in July, I decided to
+                study IT. <em>And that was what I was looking for!</em> I
+                started learning C, and after a month, I enrolled the SheCodes
+                workshops and started learning <strong>HTML</strong>,{" "}
+                <strong>CSS</strong> and <strong>Javascript</strong>. Now I'm
+                focusing in <strong>React</strong>, which is becoming a passion.
+                <br />
+                I'm a self-motivated and enthusiastic person. I have a keen
+                interest in developing new abilities and improving my skills,
+                always giving my best to not disappoint anyone. I may be new to
+                this area but I know that with my hardwork and focus, I can
+                achieve anything and deliver results with excellence.
             </p>
         </div>
     );
